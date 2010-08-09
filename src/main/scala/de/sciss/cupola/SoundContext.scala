724a0d36860d469de729f2287766542e155f5572
Sciss <contact@sciss.de>
2010-08-09 19:56:25 +0100

    ...

--- src/main/scala/de/sciss/cupola/SoundContext.scala
+++ src/main/scala/de/sciss/cupola/SoundContext.scala
@@ -1,9 +1,39 @@
 package de.sciss.cupola
 
 import collection.{ Set => ISet }
+import xml.Node
 
-case class SoundContext( proc: String,
+case class SoundContext( proc: String, settings: SoundSettings,
                          scaleStart: Double, scaleStop: Double, weight: Double,
                          minConc: Int, maxConc: Int,
-                         mutex: ISet[ String ])
-                         
+                         mutex: ISet[ String ]) {
+   def toXML : Node =
+<context>
+   <proc>{proc}</proc>
+   <settings>{settings.toXML}</settings>
+   <scaleStart>{scaleStart}</scaleStart>
+   <scaleStop>{scaleStop}</scaleStop>
+   <weight>{weight}</weight>
+   <minConc>{minConc}</minConc>
+   <maxConc>{maxConc}</maxConc>
+   <mutex>{mutex.map( s => <entry>{s}</entry> )}</mutex>
+</context>
+}
+
+sealed abstract class SoundSettings {
+   def toXML : Node
+}
+
+case class TapeSoundSettings( file: String, gain: Double, speed: Double )
+extends SoundSettings {
+   def toXML : Node =
+<tape>
+   <file>{file}</file>
+   <gain>{gain}</gain>
+   <speed>{speed}</speed>
+</tape>
+}
+
+object SoundContext {
+
+}
